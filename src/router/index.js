6a3fb7c9d16cfe7bfa1d2e4cd3ef925e51adb689
JW Park <jwpg@kakao.com>
2024-08-10 18:06:00 +0900

    30

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,17 @@
-import { createRouter, createWebHashHistory } from "vue-router";
-import flip from "@/views/flip.vue";
+import { createRouter, createWebHistory } from 'vue-router';
+import Flip from '@/views/flip.vue';
+
+const routes = [
+  {
+    path: '/',
+    name: 'Flip',
+    component: Flip,
+  },
+];
 
 const router = createRouter({
-  history: createWebHashHistory(),
-  routes: [
-    {
-      path: "/",
-      name: "flip",
-      component: flip,
-    },
-  ],
+  history: createWebHistory(), // 여기서 createWebHistory()를 사용합니다
+  routes,
 });
 
 export default router;
